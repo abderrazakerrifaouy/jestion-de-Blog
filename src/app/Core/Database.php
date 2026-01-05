57ca49164374b31a1567dd3fc662f73f847c4b65
abderrazakerrifaouy <abdrzakrifawi@gmail.com>
2026-01-05 12:29:23 +0100

    create classe database

---
+++ src/app/Core/Database.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Core;
+use PDO;
+class Database {
+    private static $connection = null;
+    private function __construct($host, $dbName, $username, $password) {
+        self::$connection = new PDO("mysql:host=$host;dbname=$dbName", $username, $password);
+        self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    public static function getInstance() {
+        if (self::$connection === null) {
+             new self("mysql", "gestioneBlog", "root", "root");
+        }
+        return self::$connection;
+    }
+}
--- src/public/index.php
+++ src/public/index.php
@@ -3,10 +3,11 @@ session_start();
     require_once __DIR__ . '/../bootstrap/autoload.php';
     require_once __DIR__ . '/../app/routes/web.php';
     use App\Core\Router;
+    use App\Core\Database;
+
 
 try {
     $router = new Router();
-
     web($router);
 
     $router->generePath();
